4c30cc3784e0b0a0ed94da83335bf5adf94e04fa
Jonathan-Day-Dev <jonday@jonday.dev>
2021-10-23 12:30:44 -0500

    initial commit

---
+++ src/components/nav/TheNavbar.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <a href="#">References</a>
+        <a href="#">Guide</a>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style scoped>
+    div {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        position: relative;
+        top: 0;
+        left: 0;
+        height: 10vh;
+        width: 100%;
+        background-color: rgb(61, 61, 163);
+    }
+
+    a {
+        color: white;
+        font-size: 24px;
+    }
+</style>
---
+++ src/components/static/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Step By Step Guide to Building VITALS Stack</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    h1 {
+        background-color: black;
+        color: red;
+        text-align: center;
+    }
+</style>
